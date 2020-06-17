d1f8800915aac8d974a808b1f18fc23ff428f0e6
Ubuntu <ubuntu@ip-172-31-50-69.ec2.internal>
2020-06-17 18:30:41 +0000

    automated batch-wise preprocessing

--- preprocess_tweets.py
+++ preprocess_tweets.py
@@ -10,28 +10,12 @@ import re
 
 DATA_DIR = '/home/ubuntu/CS224u_Final_Project/data'
 output_path = os.path.join(DATA_DIR, 'clean_unlabeled_tweets.csv')
-input_path = os.path.join(DATA_DIR,'corona_tweets_31_hydrated-002.json')
-
-# read data 
-indices = list(np.arange(400000, 500000))
-print("Reading json file")
-with open(input_path, encoding='utf-8') as json_file:
-	data = json_file.readlines()
-	data = [data[i] for i in indices]
-	print(len(data))
-	data = list(map(json.loads, data))
-print("reading complete")
-
-# Loading in the hydrated tweets into a dataframe
-tweets_df = pd.DataFrame(data)
+input_path = os.path.join(DATA_DIR, 'corona_tweets_31_hydrated-002.json')
+replace_file = os.path.join(DATA_DIR, 'hs_AsianPrejudice_hashtagsThematicReplacements.csv')
 
 # Creating a dictionary for thematic replacement
-replace_dict = pd.read_csv(os.path.join(DATA_DIR, 'hs_AsianPrejudice_hashtagsThematicReplacements.csv'))
+replace_dict = pd.read_csv(replace_file)
 dict_replace = replace_dict.set_index('Hashtag')['Replacement'].to_dict()
-
-# Create new column for clean text
-tweets_df['processed_txt'] = np.nan
-
 # function to remove url and user name, replace hashtags, lowercase the tweet
 def clean_tweet(line):
 	line = line.lower()
@@ -43,24 +27,38 @@ def clean_tweet(line):
 			clean_tokens.append('#HASHTAG')
 		else:
 			clean_tokens.append(token)
-	# collapse into a string and lower case
-	clean_text = " ".join(clean_tokens)
-	return clean_text
+	return " ".join(clean_tokens)
 
-# For re-tweets, full_text does not contain the entire tweet, so we have to go to the actual tweet to get the full$
-for index, row in tqdm(tweets_df.iterrows()):
-	line = None
-	tokens, clean_tokens = [], []
-	if tweets_df['retweeted_status'].isnull()[index]:
-		line = tweets_df['full_text'][index]
-	else:
-		line = row['retweeted_status']['full_text']
-	tweets_df.loc[index, 'processed_txt'] = clean_tweet(line)
-	if (index+1) % 10 == 0:
-		print("processed {} tweets".format(index+1))
+start_index = 500000
+end_index  = 1400000
+batch_size  = 100000
 
-columns_to_select = ['processed_txt', 'id_str']
-tweets_df.to_csv(output_path, header=False, columns=columns_to_select, mode='a', index=False)
-
-processed = pd.read_csv(output_path)
-print("total tweets processed {}".format(processed.shape))
+while(start_index < end_index):
+	# step 1 : read data
+	indices = list(np.arange(start_index, start_index+batch_size))
+	with open(input_path, encoding='utf-8') as json_file:
+		data = json_file.readlines()
+		data = [data[i] for i in indices]
+		print("Processing batch of size {}".format(len(data)))
+		data = list(map(json.loads, data))
+	tweets_df = pd.DataFrame(data)
+	tweets_df['processed_txt'] = np.nan
+	# step 2 : process all the tweets in the batch
+	for index, row in tqdm(tweets_df.iterrows()):
+		line = None
+		tokens, clean_tokens = [], []
+		if tweets_df['retweeted_status'].isnull()[index]:
+			line = tweets_df['full_text'][index]
+		else:
+			line = row['retweeted_status']['full_text']
+		tweets_df.loc[index, 'processed_txt'] = clean_tweet(line)
+		if (index+1) % 100 == 0:
+				print("in current batch, processed {} tweets".format(index+1))
+	# step 3 : write batch to file
+	columns_to_select = ['processed_txt', 'id_str']
+	tweets_df.to_csv(output_path, header=False, columns=columns_to_select, mode='a', index=False)
+	# step 4 : check if it is written properly
+	processed = pd.read_csv(output_path)
+	print("total tweets processed till now {}".format(processed.shape))
+	# step 5 : increment start_index
+	start_index += batch_size
